31924c445fffb94ebb4f731694a094ac41f93b1a
Sean Paterson <seanpaterson_25@msn.com>
2024-07-22 11:25:28 +0100

    feat: update to run with host and port from args

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
+import argparse
 from src import create_app
 
-app = create_app()
+def main():
+    parser = argparse.ArgumentParser(description='Run the application.')
+    parser.add_argument('--host', type=str, default='127.0.0.1', help='Hostname of the app')
+    parser.add_argument('--port', type=int, default=5000, help='Port of the app')
+    args = parser.parse_args()
+
+    app = create_app()
+    app.run(host=args.host, port=args.port)
+
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',
-            port=5000,
-            )
+    main()
+
--- makefile
+++ makefile
@@ -2,4 +2,4 @@ install: install
 	pip install -r requirements.txt
 
 run: run
-	python app.py
+	python app.py --host 127.0.0.1 --port 4000
